ee3342b8362dfafc71a30d17a5c09b1bf183e12c
sachanshruti <7591.shruti@gmail.com>
2024-12-06 16:36:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Shruti Sachan'
+    return 'Shruti Sachan, Roll No.: 2201330100256'
 
 if __name__ == '__main__':
     app.run(port=5002)
